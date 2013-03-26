60eb09f898c69ab6761e7fea080e5ec291886602
Felipe Amaral de Mattos <felipeamaraldemattos@gmail.com>
2013-03-26 01:27:54 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PahthsProject::Application.config.secret_token = 'df6aa97338e0c457b3c53ab86d41fca6b51daab9ddd92759d0d4048d1a2ea095170b74b55d3ae44ec0ce6cc8f77146565abd052808851a9c3dce5485e1c2ce4b'
