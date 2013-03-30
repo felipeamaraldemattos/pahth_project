e05ac760f94ef163311a0a404457b09931d9ebdb
Felipe Amaral de Mattos <felipeamaraldemattos@gmail.com>
2013-03-30 15:47:56 -0400

    201303301545 User Auth v1 done

---
+++ app/helpers/user_sessions_helper.rb
@@ -0,0 +1,38 @@
+module UserSessionsHelper
+
+
+  def current_user_session
+    return @current_user_session if defined?(@current_user_session)
+    @current_user_session = UserSession.find
+  end
+
+  def current_user
+    return @current_user if defined?(@current_user)
+    @current_user = current_user_session && current_user_session.record
+  end
+
+  def signed_in?
+    !current_user.nil?
+  end
+
+  def current_user?(user)
+    user == current_user
+  end
+
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to login_path, notice: "Please sign in."
+    end
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.url
+  end
+
+end
